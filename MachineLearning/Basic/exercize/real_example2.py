90e0bf2eff5f248bec8257414485914711f6db57
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2022-05-22 12:58:20 +0600

    Create real_example2.py

---
+++ MachineLearning/Basic/exercize/real_example2.py
@@ -0,0 +1,34 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+
+# Boston housing dataset
+boston = load_boston()
+
+df = pd.DataFrame(boston.data, columns = boston.feature_names)
+
+# Set the x-values to the nitrogen oxide concentration:
+X = df[['NOX']]
+# Y-values are the prices:
+y = boston.target
+
+# Can we do linear regression on this?
+
+linear_reg = LinearRegression()
+linear_reg.fit(X,y)
+pred = linear_reg.predict(X)
+
+plt.scatter(X,y)
+plt.plot(X,pred,color='red')
+plt.show()
+
+
+plt.scatter(X, y, alpha=0.4)
+# Plot line here:
+
+plt.title("Boston Housing Dataset")
+plt.xlabel("Nitric Oxides Concentration")
+plt.ylabel("House Price ($)")
+plt.show()
