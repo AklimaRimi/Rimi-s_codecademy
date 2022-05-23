ef610e809d7f9953a8b3a31d1d4901c13e5d0e90
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2022-05-23 14:01:54 +0600

    Update distance.py

--- MachineLearning/KNN/exercize/distance.py
+++ MachineLearning/KNN/exercize/distance.py
@@ -12,3 +12,18 @@ def distance(movie1,movie2):
 
 print(distance(star_wars,raiders))
 print(distance(star_wars,mean_girls))
+
+
+star_wars = [125, 1977, 11000000]
+raiders = [115, 1981, 18000000]
+mean_girls = [97, 2004, 17000000]
+
+def distance(movie1, movie2):
+  distance=0
+  for i in range(len(movie1)):
+    x = (movie1[i] - movie2[i]) ** 2
+    distance +=x
+  return distance**.5
+
+print(distance(star_wars,raiders))
+print(distance(star_wars,mean_girls))
