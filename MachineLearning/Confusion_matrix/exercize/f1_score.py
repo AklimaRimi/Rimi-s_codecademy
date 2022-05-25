638e83a16438f5ff42198706bf74d65cb5c5c207
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2022-05-25 11:36:30 +0600

    Create f1_score.py

---
+++ MachineLearning/Confusion_matrix/exercize/f1_score.py
@@ -0,0 +1,26 @@
+actual = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+predicted = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+for i in range(len(predicted)):
+  if actual[i] == 1 and predicted[i] == 1:
+    true_positives += 1
+  if actual[i] == 0 and predicted[i] == 0:
+    true_negatives += 1
+  if actual[i] == 0 and predicted[i] == 1:
+    false_positives += 1
+  if actual[i] == 1 and predicted[i] == 0:
+    false_negatives += 1
+
+
+recall = (true_positives)/(true_positives+false_positives)
+
+precision = (true_positives)/(true_positives+false_negatives)
+
+f_1 = (2*precision*recall)/(precision+recall) 
+
+print(recall,precision,f_1)
