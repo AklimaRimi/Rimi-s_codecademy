cc1d0b6ea82c7c738ff9fdb8d252c34868c3b4ad
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2022-05-29 23:09:07 +0600

    Create Elbow.py

---
+++ Unsupervised/exercize/Elbow.py
@@ -0,0 +1,26 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+iris = datasets.load_iris()
+
+samples = iris.data
+
+# Code Start here:
+
+num_clusters = [1,2,3,4,5,6,7,8]
+inertias = []
+
+for i in num_clusters:
+  model = KMeans(n_clusters = i)
+  model.fit(samples)
+  inertias.append(model.inertia_)
+
+plt.plot(num_clusters,inertias , '-o')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Inertia')
+plt.show()
+
